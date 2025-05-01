ab716490de78eb876718e88ddba0b9bd3e34abf9
Guillermo Call <cobbgcall@gmail.com>
2025-05-01 13:46:32 +0200

    New Message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello Skaffold!</h1>'
+    return '<h1>Hello CloudBuild!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
